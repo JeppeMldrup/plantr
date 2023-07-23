078c7ff6e5dbf377529d8ed5ff4a88fbd5742297
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-07-23 15:56:12 +0200

    feat: add functionality to the add harvest api endpoint so it adds the harvest to the database

--- front/app/api/add/route.ts
+++ front/app/api/add/route.ts
@@ -1,7 +1,21 @@
 import { NextResponse, NextRequest } from 'next/server';
+import conn from '@/lib/db';
 
 export async function POST(request: NextRequest){
     const params = request.nextUrl.searchParams;
     console.log(params);
-    return NextResponse.json({status: 200});
+
+    try{
+        let query = "INSERT INTO harvest(veg_id, date_of_harvest, amount, weight) VALUES ($1, $2, $3, $4)";
+        let values = [params.get('veg'), params.get('date'), params.get('amount'), params.get('weight')];
+
+        let result = await conn.query(query, values);
+
+        console.log(result);
+
+        return NextResponse.json({status: 200});
+    }
+    catch(error){
+        return NextResponse.error(error);
+    }
 }
