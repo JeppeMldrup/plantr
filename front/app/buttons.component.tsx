cbad0364dedd30608c55cdff5f4258336218dae3
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-08-18 10:57:55 +0200

    feat: first draft of a sliding menu on the home page

--- front/app/buttons.component.tsx
+++ front/app/buttons.component.tsx
@@ -3,6 +3,7 @@
 import { signIn, signOut } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 export const LoginButton = () => {
     return (
@@ -38,16 +39,37 @@ export const StatusBar = (props: any) => {
 export const HomeBar = (props: any) => {
     const {text} = props;
     const router = useRouter();
+
+    const [isHidden, setIsHidden] = useState(false);
+
     return (
+        <>
         <div className=' fixed w-full h-[8vh] -translate-y-[8vh] bg-gray-300 flex items-center justify-center'>
-            <div className=' h-full w-[8vh] hover:bg-gray-400 active:bg-gray-500' onClick={() => {alert("Options under construction")}}>
+            <div className=' h-full w-[8vh] hover:bg-gray-400 active:bg-gray-500' onClick={() => {setIsHidden(!isHidden)}}>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-gray-700 h-full mx-[2vh] w-[4vh] text-center self-center">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 </svg>
 
 </div>
             <span className=' text-gray-700 text-xl w-full text-center md:text-left md:ml-8 mr-[8vh]'>{text}</span>
         </div>
+        <div onClick={() => setIsHidden(!isHidden)}
+            className={` fixed top-0 left-0 w-full h-full opacity-50 bg-gray-700 ${isHidden? "-translate-x-[100vw]" : ""}`}
+        >
+        </div>
+        <div className={` fixed top-0 -left-[100vw] ${!isHidden? "translate-x-[100vw]" : "" } w-[80vw] h-full bg-gray-50 transition-all`}>
+            HERE WE GO
+        </div>
+        </>
     );
 }
+
+/**
+const SideMenu = () => {
+    return (
+        <div className={` fixed top-20 -left-[100vw] ${isHidden?}translate-x-[100vw] w-[80vw] h-full bg-green-400 transition-all`}>
+            HERE WE GO
+        </div>
+    );
+}
+*/
