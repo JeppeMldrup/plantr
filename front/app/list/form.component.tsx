75ecb9cddfd870d6f294451931bab98e26e8d7fe
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-09-03 08:03:12 +0200

    fix: fix title and invite code copying

--- front/app/list/form.component.tsx
+++ front/app/list/form.component.tsx
@@ -45,6 +45,7 @@ export const GardenInviteJoin = () => {
 export const InviteButton = () => {
     const [isGenerated, setIsGenerated] = useState(false);
     const [inviteCode, setInviteCode] = useState("");
+    const [isCopied, setIsCopied] = useState(false);
 
     const handleGenerateInvite = async () => {
         const res = await fetch('/api/joingarden');
@@ -56,13 +57,23 @@ export const InviteButton = () => {
 
     return (
         <>
-        <button className=' m-3 p-2 bg-gray-200 rounded w-1/2 hover:bg-gray-300 active:bg-gray-400 hover:cursor-pointer'
-        onClick={() => handleGenerateInvite()}>Invite to garden</button>
         {isGenerated ? (
-            <div>
-                <p>{inviteCode}</p>
+            <div className=' mx-3 h-auto w-1/2 flex'>
+                <p className=' my-3 self-center p-2 w-4/5 rounded-l border-y-2 border-l-2 border-gray-400 pl-4'>{inviteCode}</p>
+                <div className=' my-3 self-center p-2 w-1/5 bg-gray-200 hover:bg-gray-300 active:bg-gray-400 rounded-r border-y-2 border-r-2 border-gray-700'
+                    onClick={() => {navigator.clipboard.writeText(inviteCode); setIsCopied(true)}}>
+                        {isCopied ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-auto">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+</svg>
+ : 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-auto">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                      </svg>
+                      }
+                    </div>
             </div>
-        ) : null}
+        ) : <button className=' m-3 p-2 bg-gray-200 rounded hover:bg-gray-300 w-1/2 active:bg-gray-400 hover:cursor-pointer'
+        onClick={() => handleGenerateInvite()}>Invite to garden</button>}
         </>
     );
 }
