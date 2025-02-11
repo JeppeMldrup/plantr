ae3f14b23ea5c6876481c1b991745dea2bbc756f
Jeppe Moldrup <jeppemldrup@gmail.com>
2025-02-11 21:24:52 +0100

    refactor: refactor addveg page file

--- front/app/addveg/page.tsx
+++ front/app/addveg/page.tsx
@@ -1,23 +1,17 @@
-import { redirectToLogin, getLoginSession } from '@/lib/auth';
+import { redirectToLogin } from '@/lib/auth';
 import { AddVegButton } from './buttons.component';
 import { StatusBar } from '../buttons.component';
 
 export default async function Add(){
-    await redirectToLogin();
-    let session;
-    try {
-        session = await getLoginSession();
-    }
-    catch(e){
-    }
+  await redirectToLogin();
 
-    return (
-        <>
-        <StatusBar text="Add New Vegetable"/>
-        <main className=" w-full bg-gray-50 flex flex-col">
-            <p className=' self-center text-gray-700 my-2 text-2xl mx-8'>Add a new plant to your garden:</p>
-        <AddVegButton />
-        </main>
-        </>
-    )
+  return (
+    <>
+    <StatusBar text="Add New Vegetable"/>
+    <main className=" w-full bg-gray-50 flex flex-col">
+        <p className=' self-center text-gray-700 my-2 text-2xl mx-8'>Add a new plant to your garden:</p>
+    <AddVegButton />
+    </main>
+    </>
+  )
 }
