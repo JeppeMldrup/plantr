0a2d0c16862469d25bcd2407515124bf0221ae0d
Jeppe Moldrup <jeppemldrup@gmail.com>
2025-06-05 11:11:48 +0200

    feat: add standard redirect to / for login and /home after logging in

--- API/src/main/java/com/gardentally/API/Config/SecurityConfig.java
+++ API/src/main/java/com/gardentally/API/Config/SecurityConfig.java
@@ -2,11 +2,12 @@ package com.gardentally.API.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -14,7 +15,10 @@ public class SecurityConfig {
                 .requestMatchers("/", "/public/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .oauth2Login(Customizer.withDefaults());
+            .oauth2Login(oauth2 -> oauth2
+			    .loginPage("/")
+                .defaultSuccessUrl("/home")
+            );
         return http.build();
     }
 }
